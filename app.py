bf2099a3e70978638f8e7e54d37cddec933f69f0
Gisella Serafina <gisellaserafina@gmail.com>
2026-01-22 15:28:44 +0700

    refactor studico.

--- app.py
+++ app.py
@@ -139,8 +139,13 @@ st.markdown("""
 def scrape_dicoding_syllabus(url):
     try:
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.9,id;q=0.8",
+            "Referer": "https://www.dicoding.com/",
+            "Upgrade-Insecure-Requests": "1"
         }
+        
         # 1. Ambil HTML dari Link
         response = requests.get(url, headers=headers)
         response.raise_for_status()
